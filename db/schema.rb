5616413fca9176715aa8ce1314d6fca4ed4cc10c
jlee <yeoniee217@gmail.com>
2020-02-05 00:46:03 -0600

    Created customers table and ran migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_061030) do
+ActiveRecord::Schema.define(version: 2020_02_05_063546) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone_number"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
