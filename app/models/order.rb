1edb031129236966a84a924089f79076e8a93a31
jlee <yeoniee217@gmail.com>
2020-02-05 21:38:55 -0600

    Modified the association between Order and Product models.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ApplicationRecord
   # columns: id, order_date, customer_id
   belongs_to :customer # has customer_id column
-  has_many :products
+  has_many :order_products
+  has_many :products, through: :order_products
 
   validates :order_date, presence: true
   # The FK(customer_id) is also required. (implicit validation - ref to migration file for orders table)
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   # columns: id, name, price, stock_quantity
-  # belongs_to :order
-  has_many :orders
+  has_many :order_products
+  has_many :orders, through: :order_products
 
   validates :name, :price, presence: true
   validates :stock_quantity, numericality: { only_integer: true }
