e5c30313d89c25e20364db3ff582c20f8e6a652d
jlee <yeoniee217@gmail.com>
2020-02-09 03:23:53 -0600

    Created orders controller and views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/show'
   root to: 'products#index'
 
   get "products/:id", to: 'products#show', id: /\d+/, as: "product" # product_path
