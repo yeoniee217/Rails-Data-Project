bc429f14d1474834d290043b8d53a1ed5531a9e6
jlee <yeoniee217@gmail.com>
2020-02-09 03:14:04 -0600

    Implemented the products index and show actions/views.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ApplicationController
+  # GET /
   def index
+    @products = Product.all
   end
 
+  # GET /products/:id
   def show
+    @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'products#index'
+
+  get "products/:id", to: 'products#show', id: /\d+/, as: "product" # product_path
 end
