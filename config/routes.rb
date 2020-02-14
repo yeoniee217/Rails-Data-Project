d31f6179a4fa13228c08fde8390dbc3b8bb08161
jlee <yeoniee217@gmail.com>
2020-02-14 06:00:07 -0600

    Created routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
-  root to: 'products#index'
+  # get "order_products/index"
+  # get "order_products/show"
+  root to: "products#index"
 
-  get "products/:id", to: 'products#show', id: /\d+/, as: "product" # product_path
+  # get 'customers', to: 'customers#index'
+  # get 'orders', to: 'orders#index'
+
+  # get "products/:id", to: 'products#show', id: /\d+/, as: "product" # product_path
+  # get "orders/:id", to: 'orders#show', id: /\d+/, as: "order" # order_path
+  # get "customers/:id", to: 'customers#show', id: /\d+/, as: "customer" # customer_path
+
+  resources "products", only: %i[index show]
+  # GET /products     => products#index
+  # GET /products/:id => products#show
+  resources "orders", only: %i[index show]
+  resources "customers", only: %i[index show]
+  resources "categories", only: %i[index show]
+  resources "order_products", only: %i[index show]
 end
