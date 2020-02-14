19895dcfe79c5c6ddaedf0924417f843cc6f45bd
jlee <yeoniee217@gmail.com>
2020-02-14 05:55:53 -0600

    Added jquery and datatables related code.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,19 @@
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+// that code so it'll be cpompiled.
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require( 'datatables.net-dt' );
 
+// $(document).ready(function () {
+//   $('#categories_table').DataTable();
+//   $('.dataTables_length').addClass('bs-select');
+// });
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
