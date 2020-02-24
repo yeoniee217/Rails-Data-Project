52eed57b61fee802470493f3ccb27eae7e501313
jlee <yeoniee217@gmail.com>
2020-02-23 23:11:42 -0600

    Added order_num uniqueness  validation.

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,7 @@ class Order < ApplicationRecord
   has_many :products, through: :order_products
 
   validates :order_num, :order_date, presence: true
+  validates :order_num, uniqueness: true
   validates :order_num, numericality: { only_integer: true }
   # The FK(customer_id) is also required. (implicit validation - ref to migration file for orders table)
 end
